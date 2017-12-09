8cb11257d4c6b14a2a7931c77bcee61d6a567cd1
gary-thu <523570052@qq.com>
2017-12-10 00:39:13 +0800

    modify tasks model

--- src/models/tasks.js
+++ src/models/tasks.js
@@ -4,7 +4,7 @@
  * @module models/tasks
  */
 const mongoose = require('mongoose');
-const {addCreatedAt, addUpdatedAt} = require('./hooks');
+const {addCreatedAt, addUpdatedAt, addDeleted, addFileFields} = require('./hooks');
 
 /**
  * 创建`tasks` model。
@@ -15,79 +15,73 @@ const {addCreatedAt, addUpdatedAt} = require('./hooks');
  * @return {mongoose.model} `tasks` model
  */
 module.exports = function (global) {
-  const {db} = global;
+  const {db, config} = global;
 
   const statusEnum = {
-    TO_BE_SUBMITTED: 0,
-    TO_BE_ADMITTED: 1,
-    TO_BE_PUBLISHED: 2,
+    EDITING: 0,
+    SUBMITTED: 1,
+    ADMITTED: 2,
     PUBLISHED: 3,
-    COMPLETED: 4,
-    DELETED: 5
+    COMPLETED: 4
   };
-
-  const taskTypeEnum = {
-    IMAGE_COLLECTING: 0,
-    IMAGE_TAGGING: 1,
-    IMAGE_RECOGNIZING: 2
-  };
-
-  const taskSettingsSchema = new mongoose.Schema({
-  });
 
   /**
    * `tasks` schema对象，包含以下字段：
-   *  - `taskName`：字符串，必要
-   *  - `publisher`：字符串，为某user的username
-   *  - `description`：字符串，必要，任务介绍
-   *  - `tags`：字符串数组，可选
-   *  - `leftMoney`：数字，必要，剩余资金
-   *  - `spentMoney`：数字，必要，已消费的资金
+   *  - `name`：字符串，必要
+   *  - `publisher`：字符串，必要，为某user的username
+   *  - `description`：字符串，必要，任务介绍，Markdown
+   *  - `excerption`：字符串，必要，任务摘要，短文本，无Markdown
+   *  - `picture`：图片
+   *  - `pictureThumbnail`：图片缩略图
+   *  - `type`：类型，必要
+   *  - `tags`：字符串数组
    *  - `deadline`：截止时间，可选
-   *  - `createdAt`：创建时间，自动字段
-   *  - `updatedAt`：更新时间，自动字段
-   *  - `submittedAt`：提交时间，(自动字段?)
-   *  - `publishedAt`：发布时间，(自动字段?)
-   *  - `completedAt`：完成时间，(自动字段?)
+   *  - `progress`：数字，在`total`存在且大于0的时候，应当处于`[0, total]`的范围内，
+   *    且达到`total`时，任务应当处于`COMPLETED`状态
+   *  - `total`：数字，如果不存在，则该任务没有进度的概念
    *  - `status`：数字，必要，状态，可通过静态成员`statusEnum`获得所有的状态
-   *    - TO_BE_SUBMITTED：待提交
-   *    - TO_BE_ADMITTED：待审核
-   *    - TO_BE_PUBLISHED：待发布
+   *    - EDITING：待提交
+   *    - SUBMITTED：待审核
+   *    - ADMITTED：待发布
    *    - PUBLISHED：已发布
    *    - COMPLETED：已完成
-   *    - DELETED：用户被删除（用户名和邮箱将被开放给新用户注册）
+   *  - `data` 额外数据
+   *  - `createdAt`：创建时间，自动字段
+   *  - `updatedAt`：更新时间，自动字段
+   *  - `deleted` 是否被删除
    *  @class Task
    */
   const taskSchema = new mongoose.Schema({
     name: {type: String, required: true},
     publisher: {type: String, required: true},
     description: {type: String, required: true},
+    excerption: {type: String, required: true},
+    picture: {type: String},
+    pictureThumbnail: {type: String},
+    type: {type: String, required: true},
     tags: {type: [String]},
-    moneyLeft: {type: Number, required: true},
-    moneySpent: {type: Number, required: true},
-    expiredAt: {type: Date},
-    submittedAt: {type: Date},
-    publishedAt: {type: Date},
-    completedAt: {type: Date},
+    deadline: {type: Date},
+    progress: {type: Number},
+    total: {type: Number},
+    status: {type: Number, required: true},
+    data: {type: mongoose.Schema.Types.Mixed},
     createdAt: {type: Date},
     updatedAt: {type: Date},
-    status: {type: Number, required: true},
-    taskType: {type: Number, required: true},
-    settings: {type: taskSettingsSchema}
+    deleted: {type: Boolean, index: true}
   });
+
+  taskSchema.index({status: 1});
 
   /**
    * 任务的状态到编号的映射
    * @name module:models/tasks~Task.statusEnum
    */
   taskSchema.statics.statusEnum = statusEnum;
-  /**
-   * 任务的类型到编号的映射
-   * @name module:models/tasks~Task.statusEnum
-   */
-  taskSchema.statics.taskTypeEnum = taskTypeEnum;
+
   addCreatedAt(taskSchema);
   addUpdatedAt(taskSchema);
+  addDeleted(taskSchema);
+  addFileFields(taskSchema, ['picture', 'pictureThumbnail'], config['upload-dir']);
 
   return db.model('tasks', taskSchema);
 };
